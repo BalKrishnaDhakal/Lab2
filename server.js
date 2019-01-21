64372298117ae85ddb97041b0db4b1dfd0eb86b3
Bal krishna Dhakal <bdhakal1@my.centennialcollege.ca>
2019-01-20 23:11:33 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+//creational design patter or factory desing pattern
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    
+    //name and student ID is added here
+    res.end('Baby steps towards NodeJs\n\tSyed Nasir Gohary\n\t\t300937424\n\n\t\t\tSee You Next Saturday');
+  
+  });
+  
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+  
